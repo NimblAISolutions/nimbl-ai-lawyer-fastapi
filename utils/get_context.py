46c587abe03695ccabcfa5ad0b17a8c21327d08a
Hoaper <beeboplay@gmail.com>
2023-06-30 13:30:21 +0600

    contextual compression with multi documents added

--- utils/get_context.py
+++ utils/get_context.py
@@ -1,5 +1,8 @@
 from langchain.vectorstores import Milvus
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
 import os
 
 ZILLIZ_CLOUD_URI = os.environ.get("ZILLIZ_CLOUD_URI")
@@ -8,6 +11,7 @@ ZILLIZ_CLOUD_PASSWORD = os.environ.get("ZILLIZ_CLOUD_PASSWORD")
 
 
 embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+llm = OpenAI(temperature=0)
 
 database = Milvus(
     embedding_function=embeddings,
@@ -19,7 +23,8 @@ database = Milvus(
         "secure": True,
     }
 )
+compressor = LLMChainExtractor.from_llm(llm)
+compression_retriever = ContextualCompressionRetriever(base_compressor=compressor, base_retriever=database.as_retriever())
 
 def get_context_from_milvus(message: str):
-    
-    return database.similarity_search(message, k=2)[0]
+    return compression_retriever.get_relevant_documents(message)
