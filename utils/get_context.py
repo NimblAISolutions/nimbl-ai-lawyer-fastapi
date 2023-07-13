7a7074179f3da319e778c7a4aa3c84950346c812
Mukhamejan Karatayev <42678345+MukhamejanKaratayev@users.noreply.github.com>
2023-07-13 17:44:15 +0600

    remove multiquery for now and send 5 contexts

--- utils/get_context.py
+++ utils/get_context.py
@@ -11,7 +11,7 @@ ZILLIZ_CLOUD_PASSWORD = os.environ.get("ZILLIZ_CLOUD_PASSWORD")
 
 
 embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
-# llm = OpenAI(temperature=0)
+# llm = OpenAI(temperature=1.0)
 
 database = Milvus(
     embedding_function=embeddings,
@@ -26,11 +26,11 @@ database = Milvus(
 
 async def get_context_from_milvus(message: str):
 
-    llm = OpenAI(temperature=1.0)
-    contexts = llm.predict(
-        f"Вы помощник языковой модели ИИ. Ваша задача состоит в том, чтобы сгенерировать 2 разные версии данного вопроса пользователя, чтобы получить соответствующие документы из векторной базы данных. Создавая несколько точек зрения на вопрос пользователя, ваша цель — помочь пользователю преодолеть некоторые ограничения поиска сходства на основе расстояния. Оригинальный вопрос: {message} ответ должен быть в формате: 1.оригинальный вопрос 2. 1 версия 3. 2 версия")
-    print(contexts)
-    context = database.similarity_search(contexts, k=3)
-    # print(context[0:3])
+    # llm = OpenAI(temperature=1.0)
+    # contexts = llm.predict(
+    #     f"Вы помощник языковой модели ИИ. Ваша задача состоит в том, чтобы сгенерировать 2 разные версии данного вопроса пользователя, чтобы получить соответствующие документы из векторной базы данных. Создавая несколько точек зрения на вопрос пользователя, ваша цель — помочь пользователю преодолеть некоторые ограничения поиска сходства на основе расстояния. Оригинальный вопрос: {message} ответ должен быть в формате: 1.оригинальный вопрос 2. 1 версия 3. 2 версия")
+    # print(contexts)
+    context = database.similarity_search(message, k=5)
+    print(context[0:5])
     # return await compression_retriever.aget_relevant_documents(message)
-    return context[0:3]
+    return context[:5]
